b7e889bd9d2c0e00b3bdf1cab375214d4a99d158
James DeLisle <jd@luckygreenfrog.com>
2014-07-20 13:56:19 -0400

    Initial Commit

---
+++ WestWorldWithWoman/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WestWorldWithWoman
+//
+//  Created by James DeLisle on 7/20/14.
+//  Copyright (c) 2014 James DeLisle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
